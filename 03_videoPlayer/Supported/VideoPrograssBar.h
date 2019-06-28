09579b2846e2fbb3428777842c60446ef417ed52
yanshuyu <zhishuyan@gmail.com>
2019-06-28 20:08:02 +0800

    add playback progress bar, add cache loading progress. and case a unreloved crash.

---
+++ 03_videoPlayer/Supported/VideoPrograssBar.h
@@ -0,0 +1,17 @@
+//
+//  VideoPrograssBar.h
+//  03_videoPlayer
+//
+//  Created by sy on 2019/6/28.
+//  Copyright © 2019 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VideoPrograssBar : UISlider
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 03_videoPlayer/Supported/VideoPrograssBar.m
@@ -0,0 +1,21 @@
+//
+//  VideoPrograssBar.m
+//  03_videoPlayer
+//
+//  Created by sy on 2019/6/28.
+//  Copyright © 2019 sy. All rights reserved.
+//
+
+#import "VideoPrograssBar.h"
+
+@implementation VideoPrograssBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
